38022b4b6ecb1a69e6460b14b335861df239a8f6
chenruhan <339794349@qq.com>
2019-09-26 14:42:37 +0800

    更改定时初始化系统文件路径

--- src/main/java/com/bkcc/logans/task/InitTask.java
+++ src/main/java/com/bkcc/logans/task/InitTask.java
@@ -1,6 +1,5 @@
-package com.bkcc.logans.controller;
+package com.bkcc.logans.task;
 
-import com.bkcc.core.data.ViewData;
 import com.bkcc.logans.annotation.MySyncLock;
 import com.bkcc.logans.constant.RedisKeyConstant;
 import com.bkcc.logans.handler.InitTaskHandler;
@@ -8,29 +7,25 @@ import com.bkcc.util.redis.RedisUtil;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Application;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 【描 述】：初始化任务控制器
+ * 【描 述】：定时初始化系统参数
  *
  * @author 陈汝晗
  * @version v1.0
- * @since 2019-09-21 11:10
+ * @since 2019-09-26 14:35
  */
-@RestController
-@Api(value = "初始化任务控制器")
-@RequestMapping("/api/init")
-public class InitTaskController {
+@Component
+public class InitTask {
 
     /**
      * 【描 述】：任务初始化执行器
@@ -40,28 +35,27 @@ public class InitTaskController {
     @Autowired
     private InitTaskHandler taskInitHandler;
 
-
     @Autowired
     private EurekaClient eurekaClient;
 
     @Autowired
     private RedisUtil redisUtil;
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     /**
-     * 【描 述】：初始化日志分析任务配置
+     * 【描 述】：初始化日志分析任务配置--每分钟执行一次
      *
      * @since 2019-09-20 15:23:34
      */
-    @ApiOperation(value = "初始化日志分析任务配置--默认每分钟执行一次")
     @Scheduled(cron = "0 * * * * ?")
-    @PostMapping("/task")
-    public ViewData initTask() {
+    public void initTask() {
         taskInitHandler.initTask();
-        return ViewData.ok();
     }
 
     /**
-     * 【描 述】：初始化
+     * 【描 述】：初始化IP地址--每分钟执行一次
      *
      * @param
      * @return void
@@ -69,9 +63,9 @@ public class InitTaskController {
      * @since 2019/9/21 16:07
      */
     @Scheduled(cron = "0 * * * * ?")
-    @MySyncLock(key="logans:lock:client", debug = false)
+    @MySyncLock(key = "logans:lock:client:ip", debug = false, exeOnce = true)
     public void initClient() {
-        Application application = eurekaClient.getApplication("LOGANS");
+        Application application = eurekaClient.getApplication(StringUtils.upperCase(appName));
         if (application == null) {
             return;
         }
@@ -96,6 +90,6 @@ public class InitTaskController {
             redisUtil.hmSet(RedisKeyConstant.IP_KEY, cur, next);
         }
         redisUtil.expire(RedisKeyConstant.IP_KEY, 60, TimeUnit.SECONDS);
-
     }
+
 }///:~
