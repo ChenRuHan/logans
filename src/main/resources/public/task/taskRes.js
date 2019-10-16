5d94ecb6b79bbf73eff172983b5900a72cb53d5b
chenruhan <339794349@qq.com>
2019-10-16 08:35:12 +0800

    swagger注释修改，begintime注释 改为任务分析时间

--- src/main/resources/public/task/taskRes.js
+++ src/main/resources/public/task/taskRes.js
@@ -9,8 +9,8 @@ var resGridArea = {
         {field: 'id', checkbox: true},
         {field: 'taskId', title: '日志分析任务ID', width: 20},
         {field: 'orderNO', title: '任务执行单号', width: 20},
-        {field: 'beginTime', title: '任务执行开始时间', width: 20},
-        {field: 'endTime', title: '任务执行结束时间', width: 20},
+        {field: 'beginTime', title: '任务分析开始时间', width: 20},
+        {field: 'endTime', title: '任务分析结束时间', width: 20},
         {field: 'errorCode', title: '错误码', width: 15},
         {field: 'resJSON', title: '分析结果JSON字符串', width: 100},
     ]]
