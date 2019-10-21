c72c62f1f7de6074a433b1fc68db7b5b4291d19f
chenruhan <339794349@qq.com>
2019-10-21 13:56:03 +0800

    修改字段描述为数据跟踪分析

--- src/main/java/com/bkcc/logans/enums/AnsTypeEnum.java
+++ src/main/java/com/bkcc/logans/enums/AnsTypeEnum.java
@@ -3,7 +3,7 @@ package com.bkcc.logans.enums;
 public enum AnsTypeEnum {
     NOT_ANS(1, "不分析直接输出指定字段"),
     COUNT_ANS(2, "通过指定字段聚合分析"),
-    COMPARE_ANS(3, "通过指定字段对比修改数据");
+    COMPARE_ANS(3, "数据跟踪分析");
 
 
     private Integer value;
